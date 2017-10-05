353798388bc42731321f4072f3d0570290140ce7
stdio2016 <randomfrank8@gmail.com>
2017-10-05 09:54:12 +0800

    rewrite tokenizer

--- js/mml/tokenizer.js
+++ js/mml/tokenizer.js
@@ -3,8 +3,9 @@ function MmlTokenizer(code) {
   this.data = code;
   this.pos = 0;
   this.spans = [];
-  this.buf = [];
-  this.spanPos = 0;
+  this.startPos = -1;
+  this.lastPos = 0;
+  this.newlineCount = 0;
 }
 
 MmlTokenizer.prototype.isSpace = function (ch) {
@@ -16,74 +17,33 @@ MmlTokenizer.prototype.isNewline = function (ch) {
 };
 
 MmlTokenizer.prototype.next = function () {
-  var ch;
-  if (this.spanPos < this.buf.length) {
-    ch = this.buf[this.spanPos][0];
-  }
-  else {
-    var sep = [""];
-    var oldPos = this.pos;
-    var comment = true;
-    while (comment) {
-      do {
-        ch = this.data.charAt(this.pos++);
-      } while (this.isSpace(ch)) ;
-      sep.push(this.data.slice(oldPos, this.pos - 1));
-      oldPos = this.pos;
-      if (ch === ';') {
-        do {
-          ch = this.data.charAt(this.pos++);
-        } while (ch !== "" && !this.isNewline(ch)) ;
-        sep.push(this.data.slice(oldPos - 1, this.pos - 1));
-        oldPos = this.pos;
-      }
-      if (this.isNewline(ch)) {
-        sep.push('\n');
-        oldPos = this.pos;
-        comment = true;
-      }
-      else {
-        comment = false;
-      }
+  this.lastPos = this.pos;
+  do {
+    ch = this.data.charAt(this.pos++);
+    if (this.isNewline(ch)) {
+      this.newlineCount++;
     }
-    sep[0] = ch;
-    this.buf.push(sep);
+  } while (ch !== "" && (this.isNewline(ch) || this.isSpace(ch))) ;
+  if (this.startPos < 0 && ch !== "") {
+    this.startPos = this.pos - 1;
   }
-  this.spanPos++;
   return ch;
 };
 
 MmlTokenizer.prototype.rewind = function () {
-  this.spanPos--;
+  if (this.startPos === this.pos - 1) this.startPos = -1;
+  this.pos = this.lastPos;
 };
 
 MmlTokenizer.prototype.reject = function () {
-  var tok = this.buf.shift();
-  this.addSpace(tok);
-  this.spans.push({str: tok[0], type: 'invalid'});
-  this.spanPos = 0;
+  this.accept('invalid');
 };
 
 MmlTokenizer.prototype.accept = function (type) {
-  if (this.spanPos <= 0) return this.spans.length;
-  this.addSpace(this.buf[0]);
-  var pos = this.spans.length;
-  var sum = this.buf[0][0];
-  for (var i = 1; i < this.spanPos; i++) {
-    var tok = this.buf[i];
-    if (tok.length == 2) {
-      sum += tok[1].concat(tok[0]);
-    }
-    else {
-      this.spans.push({str: sum, type: type});
-      this.addSpace(tok);
-      sum = tok[0];
-    }
-  }
-  this.spans.push({str: sum, type: type});
-  this.buf = this.buf.slice(this.spanPos);
-  this.spanPos = 0;
-  return pos;
+  if (this.startPos < 0) return this.spans.length;
+  this.spans.push({pos: this.startPos, to: this.pos, type: type});
+  this.startPos = -1;
+  return this.spans.length;
 };
 
 // private!
@@ -105,7 +65,7 @@ MmlTokenizer.prototype.addSpace = function (tok) {
 MmlTokenizer.prototype.toHTML = function () {
   var doc = document.createDocumentFragment();
   var line = document.createElement('div');
-  var has = false;
+  /*var has = false;
   for (var i = 0; i < this.spans.length; i++) {
     var tok = this.spans[i];
     if (tok.type === "newline") {
@@ -121,6 +81,23 @@ MmlTokenizer.prototype.toHTML = function () {
     }
   }
   if (has)
-    doc.appendChild(line);
+    doc.appendChild(line);*/
+  var pos = 0;
+  for (var i = 0; i < this.spans.length; i++) {
+    var tok = this.spans[i];
+    if (tok.pos > pos) {
+      var txt = new Text(this.data.slice(pos, tok.pos));
+      doc.appendChild(txt);
+    }
+    var span = document.createElement('span');
+    span.className = tok.type;
+    span.textContent = this.data.slice(tok.pos, tok.to);
+    doc.appendChild(span);
+    pos = tok.to;
+  }
+  if (this.data.length > pos) {
+    var txt = new Text(this.data.slice(pos));
+    doc.appendChild(txt);
+  }
   return doc;
 };
